797cc8ba678aac5d44b2f052821699375435549f
Sergio <sergio@localhost.localdomain>
2022-04-28 16:58:55 -0700

    changed comments to english. Some changes to comments. Added a documentation file

--- MMSBM.cpp
+++ MMSBM.cpp
@@ -1,6 +1,6 @@
-//15/03/2019. MixMembership Stochastic Block Model
+//28/04/2022. This code finds latent groups of hosts and microbes
 
-//K: Grupos de pacientes. L: Grupos de microbios
+//K: Groups of hosts. L: Groups of microbes
 
 
 #include <iostream>
@@ -15,7 +15,6 @@ int MBiome;
 
 //0. VARIABLES                                                      
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 struct MatricesMemberships{
   mat theta;    //Person Memberships 
   mat eta;      //Microbe Memberships
@@ -23,18 +22,16 @@ struct MatricesMemberships{
 };
 
 
-float nan_saver=1e-10;//--->numero para prevenir singularidades en los calculos
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+float nan_saver=1e-10;//This number prevents singularities in numerical calculations
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 //1. FUNCTIONS
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-//1.1. Get Initial \eta and \theta and ps                                                               
-//================================================================================================
-MatricesMemberships GetMatrices				\
+//1.1. Get Initial \eta and \theta and ps    
+//=========================================================================================
+MatricesMemberships GetMatrices\
 (int People,int Groups_People, int Microbes, int Groups_Microbes, int Number_of_Links) {
   MatricesMemberships Init_Outputs;
   //Generate Thetas matrix
@@ -49,30 +46,30 @@ MatricesMemberships GetMatrices				\
   //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   Init_Outputs.P=zeros(Groups_People,Groups_Microbes,Number_of_Links);
    for (int i=0; i<Groups_People; i++){
-   //Crear una matriz de dimensiones (Numero de links,Numero de grupos de microbios) 
+   //Create a matrix of dimensions 'number of links x number of group of microbes
     mat M=randu(Number_of_Links,Groups_Microbes);    
-    M=normalise(M,1,0);//Normalizar sus columnas
+    M=normalise(M,1,0);//Normalise columns
     
     for (int k=0; k<Number_of_Links; k++){
-      //Para cada slice del tensor, rellenar cada una de las k filas con las probabilidades p(k,:)
-      //Iterar sobre filas
+      //For each tensor slice, fill each of the k rows with all p(k,:) probabilities.
+      //Iterate over rows
       Init_Outputs.P.slice(k).row(i)=M.row(k);	
       }}    
   //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   
   return Init_Outputs;
 }
-//================================================================================================
+//=========================================================================================
 
 
-//1.2. Get \Omega
-//================================================================================================
+//1.2. Find Omega matrix
+//=========================================================================================
 void GetOmega\
 (int People,int Groups_People, int Microbes, int Groups_Microbes, int Number_of_Links,\
  MatricesMemberships InitMatrices, mat Data, cube ListAbund, vec Tope, field<mat>* Omega) {
 
-  //1.2.0. Compute \omega numerators
-  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //1.2.0. Compute omega numerators
+  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   for (int k=0; k<Groups_People; k++){
     for (int l=0; l<Groups_Microbes; l++){
       for (int abund=0; abund<Number_of_Links; abund++){	  
@@ -81,7 +78,7 @@ void GetOmega\
   	    InitMatrices.eta( ListAbund(index,0,abund) ,l)*InitMatrices.theta( ListAbund(index,1,abund) ,k) \
   	    *InitMatrices.P(k,l,abund);
 	}}}}
-  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
   //1.2.1. Compute normalizing constants
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -90,7 +87,7 @@ void GetOmega\
     for (int l=0; l<Groups_Microbes; l++){
       Normal+=(*Omega)(k,l);
     }}
-  Normal+=nan_saver; //Evitamos singularidades
+  Normal+=nan_saver; //Avoid singularities
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
   //1.2.2. Compute final \omegas
@@ -103,75 +100,7 @@ void GetOmega\
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::  
   return;
  }
-//================================================================================================
-
-
-//1.3. Process Etas,Thetas and Ps (Marzo 2018)
-//================================================================================================
-void UpdateParametersMarch\
-(field<mat> Omega, int People, int Groups_People, int Microbes, int Groups_Microbes,\
- int Number_of_Links, mat Data, cube ListAbund, vec Tope,vec dm, vec dp, MatricesMemberships *Outputs){
-  
-  //1.3.1. Define Variables
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  //theta
-  //----------------------------------------------
-  mat theta_raw(People,Groups_People,fill::zeros);
-  //----------------------------------------------
-  //eta
-  //----------------------------------------------
-  mat eta_raw(Groups_Microbes,Microbes,fill::zeros);
-  //----------------------------------------------
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-  //1.3.2. Compute theta and eta
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  //1.3.2.1. Compute non-normalized version
-  //-----------------------------------------
-  for (int k=0;k<Groups_People; k++){
-    for (int l=0;l<Groups_Microbes; l++){      
-	theta_raw.col(k)+=sum(Omega(k,l),1);
-	eta_raw.row(l)+=sum(Omega(k,l),0);
-      }}
-  //-----------------------------------------
-  
-  //1.3.2.2. Transpose eta matrix
-  //-----------------------------------------
-  eta_raw=eta_raw.t();
-  //-----------------------------------------
-  
-  //1.3.2.3. Normalize (divide by degree)
-  //-----------------------------------------
-  //Normalize theta
-  for (int person=0;person<People;person++){
-    (*Outputs).theta.row(person)=theta_raw.row(person)/dp(person);
-  }
-  
-  //Normalize eta
-  for (int microbe=0;microbe<Microbes;microbe++){
-    (*Outputs).eta.row(microbe)=eta_raw.row(microbe)/dm(microbe);
-  }
-  //-----------------------------------------
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-  //1.3.3. Compute pkl matrices
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  for (int abund=0; abund<Number_of_Links; abund++){
-    for (int k=0; k<Groups_People; k++){
-      for (int l=0; l<Groups_Microbes; l++){
-  	//.....................................................................
-	float Numerator=0;
-  	for (int index=0; index<Tope(abund);index++){
-  	  Numerator+= Omega(k,l)( ListAbund(index,1,abund) , ListAbund(index,0,abund) );
-	}
-  	//.....................................................................
-  	(*Outputs).P(k,l,abund)=Numerator/accu(Omega(k,l));
-        }}}
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  
-  return;
-    }
-//================================================================================================
+//=========================================================================================
 
 //1.3. Process Etas,Thetas and Ps
 //================================================================================================
@@ -183,19 +112,13 @@ MatricesMemberships UpdateParameters\
   //1.3.1. Define Variables
   //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   //theta
-  //----------------------------------------------
   mat theta_raw(People,Groups_People,fill::zeros);
   Outputs.theta=zeros(People,Groups_People);
-  //----------------------------------------------
   //eta
-  //----------------------------------------------
   mat eta_raw(Groups_Microbes,Microbes,fill::zeros);
   Outputs.eta=zeros(Microbes,Groups_Microbes);
-  //----------------------------------------------
   //pkl
-  //----------------------------------------------
   Outputs.P=zeros(Groups_People,Groups_Microbes,Number_of_Links);
-  //----------------------------------------------
   //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
   //1.3.2. Compute theta and eta
@@ -261,7 +184,7 @@ double ComputeLogLikelihood\
       int microbe_L=ListAbund(index,0,abund);
       for (int k=0;k<K;k++){
 	for (int l=0;l<L;l++){
-	  Likelihood(microbe_L,person_L)+=					\
+	  Likelihood(microbe_L,person_L)+=\
   	    Update_Matrices.theta(person_L,k)*Update_Matrices.eta(microbe_L,l)*Update_Matrices.P(k,l,abund);
   	      }}
       LogLikelihoodFun+=log(Likelihood(microbe_L,person_L));
@@ -273,7 +196,6 @@ double ComputeLogLikelihood\
 //================================================================================================
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 //MAIN
@@ -311,8 +233,6 @@ int main(int argc, char *argv[])
   std::stringstream Input_FoldNumber(argv[4]);string FoldNumber;
   if (!(Input_FoldNumber >> FoldNumber)) // do the conversion
       cout<<"No Fold Number introduced"<<endl;
-  
-
   //--------------------------------------------
 
   //0.1.2. Static Arguments
@@ -328,17 +248,13 @@ int main(int argc, char *argv[])
 
   //Choose Dataset
   //--------------------------------------------------------
-
   static std::vector<std::string> Datasets;
-  //  Datasets={"Synthetic_Data/"};
   Datasets={"Leave_One_Out_S-8_Tot/", "Leave_One_Out_V-10_Tot/", "Leave_One_Out_V-22_Tot/", "Leave_One_Out_V-23_24_Tot/", "Leave_One_Out_V-25_Tot"};
 
   static std::vector<std::string> Outputs;
   Outputs={"Leave_One_Out_S-8_Tot/", "Leave_One_Out_V-10_Tot/", "Leave_One_Out_V-22_Tot/", "Leave_One_Out_V-23_24_Tot/", "Leave_One_Out_V-25_Tot"};
-  
-  //  Outputs={"Test_2022/"};
 
-  int Index=0; 	 //0: ToyData, 1: Large with categories, 2: large with quartiles, 3: microdataset
+  int Index=0; 	 //0: S-8, 1: V-10, 2: V-22, 3: V-23_24, 4: V-25
   string Dataset=Datasets[Index];
   string Output=Outputs[Index];
   //--------------------------------------------------------
@@ -377,63 +293,62 @@ int main(int argc, char *argv[])
   //1.2. Make lists of abundance-person-microbe and lists of Nans
   //=============================================================================================
   
-  //1.2.1.Calculamos el numero de slices que tiene el cubo (numero de links)
+  //1.2.1.Compute number of slices of the cube (hypermatrix). This number is the number of types of links
   //------------------------------------------------------------------------
   int Links= Data.max() - Data.min() +1;
   //------------------------------------------------------------------------
   
-  //1.2.2.Calculamos que tipo de link tiene el mayor numero de elementos y cuantos elementos tiene
+  //1.2.2. Compute most frequent link and how many elements does it contain
   //------------------------------------------------------
   vec number_of_links=zeros<vec>(Links);
   
   for (int abundance=0;abundance<Links;abundance++){
-    uvec Census=find(Data==abundance);      //Vector de indices de las ocurrencias de una abundancia
-    int  Population=Census.n_rows;          //Dimension del vector (numero de links)
-    number_of_links(abundance)=Population;  //Rellenar el vector de abundancias
+    uvec Census=find(Data==abundance);      //Vector of indices of instances of an abundance
+    int  Population=Census.n_rows;          //Vector dimension (number of links)
+    number_of_links(abundance)=Population;  //Fill vector of abundances
   }
  
-  int Top_Abundance=number_of_links.max(); //Coger el maximo del vector
+  int Top_Abundance=number_of_links.max(); //Maximum of vector
   //------------------------------------------------------
 
 
-  //1.2.3. Guardamos la lista de links en forma de cubo y la de nans en forma de matriz 
+  //1.2.3. Save list of links in a cube. Save list of nans as a matrix
   //-----------------------------------------------------------------------------------
 
-  //1.2.3.1. Definir cubo y contadores
+  //1.2.3.1. Define cube and counters
   //................................................................
-  cube ListAbund(Top_Abundance,2,Links,fill::zeros);//Crear el cubo
-  vec contador=zeros<vec>( Links );		     //Crear vector de contadores
+  cube ListAbund(Top_Abundance,2,Links,fill::zeros); //Define cube
+  vec contador=zeros<vec>( Links );		     //Create vector of counters
   //................................................................
   
-  //1.2.3.2. Definir lista de nans y contador
+  //1.2.3.2. Define nan list and counter
   //..........................................
   uvec RecordOfNans=find_nonfinite(Data); //Vector with indices of nans in the train matrix
   int NanRows=RecordOfNans.n_rows;        //Number of nans
     
-  mat ListNans(NanRows,2,fill::zeros);//LISTA DE NANS. 
+  mat ListNans(NanRows,2,fill::zeros);//NANS LIST 
   int count_nans=0;
   //..........................................
 
 
-  //1.2.3.3. Rellenar listas
+  //1.2.3.3. Fill Lists
   //............................................................
   for(int row=0;row<MBiome;row++){
     for(int col=0;col<People;col++){
       
-      //Nans: Saltarlos y guardar lista
+      //Nans: Skipt them and safe list
       //...............................
       if ( isnan( Data(row,col)) ){
-	ListNans(count_nans,0)=row; //Fila donde vive el nan
-	ListNans(count_nans,1)=col; //Columna donde vive el nan
+	ListNans(count_nans,0)=row; //Row where nan lives
+	ListNans(count_nans,1)=col; //Column where nan lives
 	count_nans+=1;
 	  continue;
 	}
       //...............................
 
-      ListAbund( contador( Data(row,col) ) ,0, Data(row,col) )=row; //Primera columna (Microbio)
-      ListAbund( contador( Data(row,col) ) ,1, Data(row,col) )=col; //Segunda columna (Persona)
-      contador( Data(row,col) )+=1; //Activar contador que toque
-
+      ListAbund( contador( Data(row,col) ) ,0, Data(row,col) )=row; //First columna (Microbe)
+      ListAbund( contador( Data(row,col) ) ,1, Data(row,col) )=col; //Second column (Host)
+      contador( Data(row,col) )+=1; //Activate corresponding counter
     }}
   //............................................................
 
@@ -445,13 +360,13 @@ int main(int argc, char *argv[])
   //1.3. Make list of neighbors of each node
   //=============================================================================================
 
-  //1.3.1. Crear lista como vector y rellenarlo por defecto
+  //1.3.1. Create list as a vector and fill it automatically
   //----------------------------------------------------
   vec Nodes_Microbe(MBiome);Nodes_Microbe.fill(People);
   vec Nodes_People(People) ;Nodes_People.fill(MBiome);
   //----------------------------------------------------
 
-  //1.3.2. Completar lista. A cada elemento le quitamos la cantidad de nans detectados
+  //1.3.2. Complete list. We remove nans from each corresponding element
   //----------------------------------------------------
   for (int i=0; i<NanRows;i+=1)
     { Nodes_Microbe( ListNans(i,0) )-=1;
@@ -487,7 +402,7 @@ int main(int argc, char *argv[])
   //===========================================
 
   //1.5. Expectation-Maximization Loop
-  //=============================================================================================================
+  //================================================================================================
 
   //1.5.1. Declare variables
   //::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -513,7 +428,7 @@ int main(int argc, char *argv[])
   //::::::::::::::::::::::::::::::::::::::::::::::::::::
 
   //1.5.3. Loop
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   timer.tic();
   for (int Iterations=0;Iterations<Limit;Iterations++){
 
@@ -570,8 +485,9 @@ int main(int argc, char *argv[])
   cout << "number of seconds: " << n << endl;
 
 
-  //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-  //1.6. Decide whether to print Outputs?
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+  //1.6. Decide whether to prompt Outputs during execution
   //==========================================================
   int outputs=0;
   if (outputs==1){
@@ -595,8 +511,8 @@ int main(int argc, char *argv[])
   //1.7. Write model to file
   //=========================================================================
   string ModelParameters;
-  ModelParameters= \
-  OutputPath + FoldNumber + "_" + "Seed_" + strSeed + "_K_" + strK + "_L_" + strL + "_Parameters_Test.txt";
+  ModelParameters=OutputPath + FoldNumber + "_" +\
+  "Seed_" + strSeed + "_K_" + strK + "_L_" + strL + "_Parameters_Test.txt";
 
   ofstream Parameters;
   Parameters.open(ModelParameters);
@@ -607,7 +523,8 @@ int main(int argc, char *argv[])
   //1.8. Write scores to file
   //=========================================================================
   string OutputFile;
-  OutputFile= OutputPath + FoldNumber + "_" + "Seed_" + strSeed + "_" + "K_" + strK + "L_" + strL + "_scores_Test.txt";
+  OutputFile= OutputPath + FoldNumber + "_" + \
+  "Seed_" + strSeed + "_" + "K_" + strK + "L_" + strL + "_scores_Test.txt";
     
   ofstream scores;
   scores.open(OutputFile);
